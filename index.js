d1cebc75139ad98ef6489aa5face09075637dcf3
PapaFreddie <fred.papa@student.moringaschool.com>
2021-11-20 12:59:54 +0300

    added paragraphs, images

---
+++ index.js
@@ -0,0 +1,13 @@
+$('#btn1').on('click', function(){
+    $('.paragraph').toggle(3000);
+
+});
+
+$('#btn2').on('click', function(){
+    $('.paragraph1').toggle(3000);
+
+});
+$('#btn3').on('click', function(){
+    $('.paragraph2').toggle(3000);
+
+});
