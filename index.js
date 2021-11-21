e42e115e7f189514d83725d969ded01fbd2cda27
PapaFreddie <fred.papa@student.moringaschool.com>
2021-11-21 18:40:09 +0300

    update javaScript file

--- index.js
+++ index.js
@@ -3,20 +3,35 @@ $('#btn0').on('click', function(){
     
 
 });
+$('#btn1').on('click');
+$('.paragraph').hide();
 $('#btn1').on('click', function(){
-    $('.paragraph').toggle(1000);
-    $('#btn1').on('click', function(){
-        $('img').hide()
-
+    $('.paragraph').toggle(1000,function(){
+        $('#dicon').hide(1000);
     });
-
 });
 
+
+
+$('#btn2').on('click');
+$('.paragraph1').hide();
 $('#btn2').on('click', function(){
-    $('.paragraph1').toggle(1000);
-
+    $('.paragraph1').toggle(1000,function(){
+        $('#dev').hide(1000);
+    });
 });
+
+
+$('#btn3').on('click');
+$('.paragraph2').hide();
 $('#btn3').on('click', function(){
-    $('.paragraph2').toggle(1000);
-
+    $('.paragraph2').toggle(1000,function(){
+        $('#product').hide(1000);
+    });
 });
+
+
+
+
+
+
