6ca41b62dade6fc948671b238e30f0125c8fc1f4
PapaFreddie <fred.papa@student.moringaschool.com>
2021-11-22 08:13:22 +0300

    update contact us section

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
+//Click mouse function
 $('#btn0').on('click', function(){
     $('.about').toggle(1000);
     
 
 });
+
+//What we do section
 $('#btn1').on('click');
 $('.paragraph').hide();
 $('#btn1').on('click', function(){
-    $('.paragraph').toggle(1000,function(){
-        $('#dicon').hide(1000);
+    $('.paragraph').toggle(2000,function(){
+        $('#dicon').hide(200);
     });
 });
 
@@ -16,8 +20,8 @@ $('#btn1').on('click', function(){
 $('#btn2').on('click');
 $('.paragraph1').hide();
 $('#btn2').on('click', function(){
-    $('.paragraph1').toggle(1000,function(){
-        $('#dev').hide(1000);
+    $('.paragraph1').toggle(2000,function(){
+        $('#dev').hide(200);
     });
 });
 
@@ -25,12 +29,20 @@ $('#btn2').on('click', function(){
 $('#btn3').on('click');
 $('.paragraph2').hide();
 $('#btn3').on('click', function(){
-    $('.paragraph2').toggle(1000,function(){
-        $('#product').hide(1000);
+    $('.paragraph2').toggle(2000,function(){
+        $('#product').hide(200);
     });
 });
 
+//Contact us section
+$('#submit').load('text.html', function(responseTxt, statusTxt, xhr){
+    if(statusTxt == 'success'){
+        alert('Thanks for reaching us');
+    }else if(statusTxt == 'error'){
+        alert('Error: '+xhr.statusText);
+    }
 
+});
 
 
 
